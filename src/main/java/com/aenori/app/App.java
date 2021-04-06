2bb5255fc5ab6a678d9944f24d7e00541a5cc280
Nicolas <Nicolas Rousset>
2021-04-06 20:22:07 +0200

    Testing CI

--- src/main/java/com/aenori/app/App.java
+++ src/main/java/com/aenori/app/App.java
@@ -1,5 +1,7 @@
 package com.aenori.app;
 
+import org.apache.commons.math3.linear.RealMatrix;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    public static RealMatrix addRealMatrix(RealMatrix m1, RealMatrix m2)
+    {
+    	return m1.add(m1);
     }
 }
--- src/test/java/com/aenori/app/AppTest.java
+++ src/test/java/com/aenori/app/AppTest.java
@@ -2,6 +2,8 @@ package com.aenori.app;
 
 import static org.junit.Assert.assertTrue;
 
+import org.apache.commons.math3.linear.Array2DRowRealMatrix;
+import org.apache.commons.math3.linear.RealMatrix;
 import org.junit.Test;
 
 /**
@@ -16,5 +18,16 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    
+    @Test
+    public void shouldAddMatrix()
+    {
+    	RealMatrix m1 = new Array2DRowRealMatrix(new double[][] {{1, 2}, {3, 4}});
+    	RealMatrix m2 = new Array2DRowRealMatrix(new double[][] {{2, 3}, {-1, -2}});
+    	
+    	RealMatrix m3 = new Array2DRowRealMatrix(new double[][] {{3, 5}, {2, 2}});
+    	
+    	assertTrue(m3.subtract(App.addRealMatrix(m1, m2)).getNorm() < 1e-7);
     }
 }
